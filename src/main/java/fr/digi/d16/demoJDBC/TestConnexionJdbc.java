26bd63f15288c656490fb39e1188a738afde53d6
sopurdey <a_purdey@hotmail.com>
2021-12-02 14:50:59 +0100

    TP02 connection bdd distante jdbc

--- src/main/java/fr/digi/d16/demoJDBC/TestConnexionJdbc.java
+++ src/main/java/fr/digi/d16/demoJDBC/TestConnexionJdbc.java
@@ -3,12 +3,20 @@ package fr.digi.d16.demoJDBC;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class TestConnexionJdbc {
 
-	private static final String DB_URL = "jdbc:mariadb://localhost:3306/compta";
-	private static final String DB_LOGIN = "root";
-	private static final String DB_PWD = "root";
+	private static final String DB_URL;
+	private static final String DB_LOGIN;
+	private static final String DB_PWD;
+
+	static {
+		ResourceBundle resourceBundle = ResourceBundle.getBundle("db");
+		DB_URL = resourceBundle.getString("jdbc.db.url");
+		DB_LOGIN = resourceBundle.getString("jdbc.db.user");
+		DB_PWD = resourceBundle.getString("jdbc.db.password");
+	}
 
 	public static void main(String[] args) {
 		try (Connection connection = DriverManager.getConnection(DB_URL, DB_LOGIN, DB_PWD)) {
