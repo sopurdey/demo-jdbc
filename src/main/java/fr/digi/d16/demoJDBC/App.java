e0341feec8740c4a3dd2da9325d7b19bb592df8b
sopurdey <a_purdey@hotmail.com>
2021-12-03 17:36:42 +0100

    TP DAO / requêtes préparées màj

--- src/main/java/fr/digi/d16/demoJDBC/App.java
+++ src/main/java/fr/digi/d16/demoJDBC/App.java
@@ -1,13 +1,27 @@
 package fr.digi.d16.demoJDBC;
 
+import java.sql.SQLException;
+
+import fr.diginamic.jdbc.dao.FournisseurDaoJdbc;
+import fr.diginamic.jdbc.dao.IDAO;
+import fr.diginamic.jdbc.entites.Fournisseur;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		creerFournisseur();
+	}
+
+	private static void creerFournisseur() {
+		IDAO<Fournisseur, Integer> fournisseurDao = new FournisseurDaoJdbc();
+		Fournisseur fournisseurTest = new Fournisseur(4, "L'Espace Création");
+		try {
+			fournisseurDao.insert(fournisseurTest);
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	}
 }
